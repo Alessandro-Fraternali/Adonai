d3fc4a0fdb0bc7bb5dcdc66e94dd91d47063fd67
Alessandro-Fraternali <alessandro.fraternali@parsec326.it>
2024-12-25 22:23:03 +0100

    fix produzione

--- src/app/Contattaci/page.tsx
+++ src/app/Contattaci/page.tsx
@@ -1,56 +1,10 @@
 import { Box } from "@mui/material";
-// import Versatile from "../components/Versatile";
-import versatiletest from "../texts/versatiletest.json";
-import Porygon from "../components/Porygon";
-
-interface Mission {
-  title?: string;
-  description?: string;
-  images?: Array<string>;
-  imageLeft?: boolean;
-  button?: string;
-}
-
-interface Missions {
-  [key: string]: Mission;
-}
-
-interface MissioniData {
-  title: string;
-  versatili: Missions;
-}
-
-const missioniData: MissioniData = versatiletest;
 
 export default function Contattaci() {
   return (
     <>
       <Box>
-        <h1 style={{ marginBottom: 20 }}>{missioniData.title}</h1>
-        {Object.keys(missioniData.versatili).map((key) => {
-          const initiative = missioniData.versatili[key];
-          return (
-            <>
-              <Porygon
-                key={key}
-                title={initiative.title}
-                description={initiative.description}
-                images={initiative.images}
-                imageLeft={initiative.imageLeft}
-                button={initiative.button}
-              />
-              <span
-                style={{ textAlign: "center", display: "block", width: "100%" }}
-              >
-                <br />
-                <br />
-                ------ fine blocco ------
-                <br />
-                <br />
-              </span>
-            </>
-          );
-        })}
+        <h1 style={{ marginBottom: 20 }}>Contattaci</h1>
       </Box>
     </>
   );
