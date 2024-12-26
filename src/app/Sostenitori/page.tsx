59a2117e17cc683409b591b4c8421f3c7695c0df
Alessandro-Fraternali <alessandro.fraternali@parsec326.it>
2024-12-26 15:25:41 +0100

    reimpaginati collaboratori e sostenitori

--- src/app/Sostenitori/page.tsx
+++ src/app/Sostenitori/page.tsx
@@ -1,8 +1,7 @@
-import { Box, Grid2, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import CarouselBrands from "../components/CarouselBrands";
 import sostenitori from "../texts/sostenitori.json";
-import Image from "next/image";
-import Description from "../components/Description";
+import Porygon from "../components/Porygon";
 
 type Sostenitore = {
   url: string;
@@ -16,7 +15,7 @@ interface SostenitoriData {
   title: string;
   description: string;
   ringraziamenti: string;
-  ringraziamenti_img: string;
+  ringraziamenti_img: string[];
   sostenitori: Sostenitori;
 }
 
@@ -25,38 +24,15 @@ const sostenitoriData: SostenitoriData = sostenitori;
 export default function Sostenitori() {
   return (
     <Box>
-      <Description text={sostenitoriData.description} />
+      <br />
+      <br />
+      <Porygon
+        title={sostenitoriData.title}
+        description={sostenitoriData.description}
+        gallery={sostenitoriData.ringraziamenti_img}
+      />
+      <br />
       <CarouselBrands images={sostenitoriData.sostenitori} />
-      <Box
-        sx={{
-          margin: "auto",
-          textAlign: "center",
-        }}
-      >
-        <Grid2 container flexDirection={"column"}>
-          <Grid2 margin={"auto"} size={{ xs: 12, md: 6 }}>
-            <Typography variant="h5" marginBottom={3}>
-              {sostenitoriData.ringraziamenti}
-            </Typography>
-          </Grid2>
-          <Grid2
-            margin={"auto"}
-            size={{ xs: 12, md: 6 }}
-            sx={{ position: "relative", height: "300px" }} // Set a specific height for the container
-          >
-            <Image
-              src={sostenitoriData.ringraziamenti_img}
-              alt={sostenitoriData.ringraziamenti}
-              layout="fill" // Allows the image to fill the container
-              objectFit="cover" // Ensures the image covers the container
-              style={{
-                top: 0,
-                left: 0,
-              }}
-            />
-          </Grid2>
-        </Grid2>
-      </Box>
     </Box>
   );
 }
